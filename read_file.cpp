81b4312234edc282b2e853f380795f868e82f407
Arkadiy Pozharskiy <50653532+Kr1eg3@users.noreply.github.com>
2021-12-05 22:04:35 +0000

    added SSIM

--- read_file.cpp
+++ read_file.cpp
@@ -9,16 +9,17 @@
  *
  *  **************************************************************************
  *  ********************** ИСПОЛЬЗОВАНИЕ ПРОГРАММЫ ***************************
+ *  **                                                                      **
  *  **  1. Подразумевается, что у вас имеется 2 папки с исходными файлами.  **
  *  **     Если нет - то порядок получения этих папок описан в пункте       **
- *  **       "Подготовительные действия" по ссылке:                         **
+ *  **     "Подготовительные действия" по ссылке:                           **
  *  **        << https://github.com/Kr1eg3/test_for_ViCueSoft >>            **
  *  **                                                                      **
- *  **    2. После компиляции исполняемого файла, в консоли/терминале       **
- *  **       необходимо прописать следующую команду:                        **
+ *  **  2. После компиляции исполняемого файла, в консоли/терминале         **
+ *  **     необходимо прописать следующую команду:                          **
  *  **           ~$ ./app <Source Folder> <Test Folder> <Num of frames>     **
  *  **                                                                      **
- *  **       Пример:                                                        **
+ *  **     Пример:                                                          **
  *  **           ~$ ./app Source Test 260                                   **
  *  **                                                                      **
  *  **************************************************************************
@@ -32,6 +33,8 @@
 #include <sstream>
 #include <stdlib.h> 
 #include <math.h>
+#include <iomanip>
+#include <fstream>
 
 // Подключение хедера std_image.h
 extern "C" {
@@ -59,8 +62,20 @@ int main( int argc, char *argv[] )
     conv << argv[3];
     conv >> numFrames;
     
-    // Инициализация среднего PSNR
-    float AvgPSNR = 0.f;
+    // Инициализация среднего PSNR и SSIM
+    double AvgPSNR = 0.0;
+    double AvgSSIM = 0.0;
+
+    // Инициализация констант
+    const double k1 = 0.01;
+    const double k2 = 0.03;
+    const int L = 255;
+    const double c1 = pow((k1 * L), 2);
+    const double c2 = pow((k2 * L), 2);
+    
+    char buffer[0x100];
+    std::snprintf(buffer, sizeof(buffer), "results_for_%s.txt", argv[2]); 
+    FILE* file = fopen(buffer, "w");
 
     // Главный цикл который проходится по фреймам
     for (int frame = 1; frame <= numFrames; frame++)
@@ -84,26 +99,69 @@ int main( int argc, char *argv[] )
       load_check(successTst, testFile);
 
       // Расчёты метрик
-      float mod_under_mse = 0.f;
+      double mod_under_mse = 0.0;
+      double avg_x = 0.0;
+      double avg_y = 0.0;
+      double sigma_x = 0.0;
+      double sigma_y = 0.0;
+      double sigma_xy = 0.0;
       for (int x = 0; x < width; x++)
       {
         for (int y = 0; y < height; y++)
         {
           size_t index = 1 * (y * width + x);
           auto mod_under_mse_single = pow(abs(static_cast<int>(refImage[index]) - static_cast<int>(tstImage[index])), 2);
+          avg_x += static_cast<int>(refImage[index]);
+          avg_y += static_cast<int>(tstImage[index]);
           mod_under_mse += mod_under_mse_single;
         } 
       }
+      avg_x = avg_x / (height * width);
+      avg_y = avg_y / (height * width);
       
-      auto mse = mod_under_mse / (height * width);
-      auto psnr = 10 * log10((255 * 255) / mse);
-      AvgPSNR += psnr;
-    }
+      //std::cout <<"mu_x = " <<  avg_x << "mu_y = " <<  avg_y << std::endl;
 
+      double mse = mod_under_mse / (height * width);
+      double psnr = 10 * log10(pow(L, 2) / mse);
+      
+      fprintf(file, "Frame %d PSNR = %f\n", frame, psnr);
+    
+      AvgPSNR += psnr;
+    
+      double under_summ_sqx = 0.0f;
+      double under_summ_sqy = 0.0f;
+      double under_summxy = 0.0f;
+      for (int x = 0; x < width; x++)
+      {
+        for (int y = 0; y < height; y++)
+        {
+          size_t index_ = 1 * (y * width + x);
+          under_summxy += (static_cast<int>(refImage[index_]) - avg_x) * (static_cast<int>(tstImage[index_]) - avg_y);
+          under_summ_sqx += pow( (static_cast<int>(refImage[index_]) - avg_x), 2 );
+          under_summ_sqy += pow( (static_cast<int>(tstImage[index_]) - avg_y), 2 );
+        }
+      }
+
+      sigma_x = sqrt( (1 / (height * width - 1) * under_summ_sqx) );
+      sigma_y = sqrt( (1 / (height * width - 1) * under_summ_sqy) );
+      sigma_xy = 1 / (height * width - 1) * under_summxy;
+
+      AvgSSIM += 2 * (avg_x * avg_y + c1) * (2 * sigma_xy + c2) / ( (pow(avg_x, 2) + pow(avg_y, 2) + c1) * (pow(sigma_x, 2) + pow(sigma_y, 2) + c2) );
+    }
+    
+    // Конец главного цикла и расчёт средних значений метрик
+    AvgSSIM = AvgSSIM / numFrames;
     AvgPSNR = AvgPSNR / numFrames;
+    //double AvgDSSIM = (1 - AvgSSIM) / 2;
+    
+    fprintf(file, "Avg PSNR = %f\n", AvgPSNR);
+    fprintf(file, "Avg SSIM = %f\n", AvgSSIM);
+    fclose(file);
 
     // Вывод метрик
     std::cout << "Avg PSNR = " << AvgPSNR << std::endl;
+    std::cout << "Avg SSIM = " << AvgSSIM << std::endl;
+    //std::cout << "Avg AvgDSSIM = " << AvgDSSIM << std::endl;
 
     return 0;
 }
